afb0fd339550180da93e0f7ecc58d8b1fd183a9d
Pierluigi Tassi <pierluigi.tassi@regione.emilia-romagna.it>
2019-01-25 17:40:30 +0100

    Primo layout accettabile :)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import '@fortawesome/fontawesome-free';
+import $ from 'jquery';
+require('@fortawesome/fontawesome-free/js/all');
+
+$(document).ready(function(){
+    console.log('so pronto');
+});
